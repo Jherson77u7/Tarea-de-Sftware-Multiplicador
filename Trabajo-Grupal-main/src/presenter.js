78cfd983112110ce0e978a8e0f26f0c19afed483
Jherson <Jherson.chambi@ucb.edu.bo>
2024-08-15 00:02:35 +0200

    Version2

---
+++ Trabajo-Grupal-main/src/presenter.js
@@ -0,0 +1,22 @@
+import sumar from "./sumador";
+import multiplicar from "./Multiplicador.js";
+
+const first = document.querySelector("#primer-numero");
+const second = document.querySelector("#segundo-numero");
+const sumarButton = document.querySelector("#sumar-button");
+const multiplicarButton = document.querySelector("#multiplicar-button");
+const div = document.querySelector("#resultado-div");
+
+sumarButton.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+multiplicarButton.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+});
